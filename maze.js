873deeb93335c2a985f3e97ed10a7393d86c5bbc
Cedrick Garcia <icedgarcia@gmail.com>
2021-12-11 02:45:33 +0800

    error in algorithm

--- maze.js
+++ maze.js
@@ -42,6 +42,26 @@ class Maze {
         grid[r][c]._show(this.size, this.rows, this.columns);
       }
     }
+    let next = current._checkNeighbors();
+    if (next) {
+      next.visited = true;
+      this.stack.push(current);
+      current._highlight(this.columns);
+
+      current._removeWall(current, next);
+
+      current = next;
+    } else if (this.stack.length > 0) {
+      let cell = this.stack.pop;
+      current = cell;
+      current._highlight(this.columns);
+    }
+    if (this.stack.length === 0) {
+      return;
+    }
+    window.requestAnimationFrame(() => {
+      this._draw();
+    });
   }
 }
 
@@ -61,6 +81,31 @@ class Cell {
       bottomWall: true,
       leftWall: true,
     };
+  }
+  _checkNeighbors() {
+    let grid = this.parentGrid;
+    let row = this.rowNum;
+    let col = this.colNum;
+    let neighbors = [];
+
+    // Side area of canvas will return undefined to
+    // side area out of grid scope
+    let top = row !== 0 ? grid[row - 1][col] : undefined;
+    let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
+    let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
+    let left = col !== 0 ? grid[row][col - 1] : undefined;
+
+    if (top && !top.visited) neighbors.push(top);
+    if (right && !right.visited) neighbors.push(right);
+    if (bottom && !bottom.visited) neighbors.push(bottom);
+    if (left && !left.visited) neighbors.push(left);
+
+    if (neighbors.length != 0) {
+      let random = Math.floor(Math.random() * neighbors.length);
+      return neighbors[random];
+    } else {
+      return undefined;
+    }
   }
 
   _drawTopWall(x, y, size, rows, columns) {
@@ -104,14 +149,39 @@ class Cell {
       ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
     }
   }
-}
 
-// for (let a = 0; a < 3; a++) {
-//   console.log('a=', a);
-//   for (let b = 0; b < 3; b++) {
-//     console.log('b=', b);
-//   }
-// }
+  _highlight(columns) {
+    let x = (this.colNum * this.parentSize) / columns + 1;
+    let y = (this.rowNum * this.parentSize) / columns + 1;
+
+    ctx.fillStyle = 'green'; //Color of visited color
+    ctx.fillRect(
+      x + 1,
+      y + 1,
+      this.parentSize / columns - 3,
+      this.parentSize / columns - 3
+    );
+  }
+  _removeWall(cell1, cell2) {
+    // compare cell on x axis
+    let x = (cell1.colNum = cell2.colNum);
+    if (x == 1) {
+      cell1.walls.leftWall = false;
+      cell2.walls.rightWall = false;
+    } else if (x == -1) {
+      cell1.walls.rightWall = false;
+      cell2.walls.leftWall = false;
+    }
+    let y = (cell1.rowNum = cell2.rowNum);
+    if (y == 1) {
+      cell1.walls.topWall = false;
+      cell2.walls.bottomWall = false;
+    } else if (y == -1) {
+      cell1.walls.bottomWall = false;
+      cell2.walls.topWall = false;
+    }
+  }
+}
 
 let newMaze = new Maze(500, 10, 10);
 newMaze._setup();
